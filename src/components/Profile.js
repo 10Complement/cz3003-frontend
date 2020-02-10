71d091aeb9e08f321d179e80aaaba56b86a5f893
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-02-10 18:45:19 +0800

    Demo purpose

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,11 @@
 import React from "React";
 
 export default function() {
-  // This is the profile branch
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+	// This is the profile branch
+	// Hello Yamin
+	return (
+		<>
+			<h1>Hello world</h1>
+		</>
+	);
 }
