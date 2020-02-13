7c580539851c7a203aaae4e1742df9eb6bb25fae
Guang Wei <guangwei.ngoh@gmail.com>
2020-02-14 00:29:47 +0800

    Added world feature branch

--- src/components/World/WorldContainer.js
+++ src/components/World/WorldContainer.js
@@ -2,25 +2,26 @@ import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function() {
-	const { wID } = useParams();
+  const { wID } = useParams();
 
-	/* State Declaration */
-	// const { count, setCount } = React.useState(0);
+  /* State Declaration */
+  // const { count, setCount } = React.useState(0);
 
-	/* Called only once whenever component is mounted */
-	useEffect(() => {
-		// Perform API calls
-		// Update states
-	}, []);
+  /* Called only once whenever component is mounted */
+  useEffect(() => {
+    // Perform API calls
+    // Update states
+  }, []);
 
-	return (
-		<>
-			<h1>This is WorldContainer</h1>
-			<p>
-				You are in World ID: {wID}
-				<br />
-				Change the browser URL parameter and see the ID change
-			</p>
-		</>
-	);
+  return (
+    <>
+      <h1>This is WorldContainer</h1>
+      {/* World Comment */}
+      <p>
+        You are in World ID: {wID}
+        <br />
+        Change the browser URL parameter and see the ID change
+      </p>
+    </>
+  );
 }
