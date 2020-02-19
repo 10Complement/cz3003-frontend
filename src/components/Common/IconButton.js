c331e1c7e36b990848a91b4cdf98ed43d1ad8c3e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-02-20 00:02:34 +0800

    Fixed triple equal operator

--- src/components/Common/IconButton.js
+++ src/components/Common/IconButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { FaHome } from 'react-icons/fa';
-import { FaMedal } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
+import { FaMedal } from "react-icons/fa";
 import { AiFillTrophy } from "react-icons/ai";
 import { AiOutlineStar } from "react-icons/ai";
 
@@ -9,20 +9,28 @@ import { AiOutlineStar } from "react-icons/ai";
 	return <Button>{props.label}</Button>;
 }    */
 export default function(props) {
-	if(props.label = "Home")
-		return (		
-			<Button variant="outline-dark"><FaHome /></Button>
-	)
-	if(props.label = "Star")
-		return (		
-			<Button variant="outline-dark"><AiOutlineStar /></Button>
-	)
-	if(props.label = "Medal")
-		return (		
-			<Button variant="outline-dark"><FaMedal /></Button>
-	)
-	if(props.label = "Trophy")
-		return (		
-			<Button variant="outline-dark"><AiFillTrophy /></Button>
-	)
+	if (props.label === "Home")
+		return (
+			<Button variant="outline-dark">
+				<FaHome />
+			</Button>
+		);
+	if (props.label === "Star")
+		return (
+			<Button variant="outline-dark">
+				<AiOutlineStar />
+			</Button>
+		);
+	if (props.label === "Medal")
+		return (
+			<Button variant="outline-dark">
+				<FaMedal />
+			</Button>
+		);
+	if (props.label === "Trophy")
+		return (
+			<Button variant="outline-dark">
+				<AiFillTrophy />
+			</Button>
+		);
 }
