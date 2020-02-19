ca507911f2001c3c8fd8983823130abe51336574
Ash <ashwariii97@gmail.com>
2020-02-19 19:49:45 +0800

    Created icon and table component

--- src/components/Common/IconButton.js
+++ src/components/Common/IconButton.js
@@ -1,6 +1,28 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import { FaHome } from 'react-icons/fa';
+import { FaMedal } from 'react-icons/fa';
+import { AiFillTrophy } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 
-export default function(props) {
+/* export default function(props) {
 	return <Button>{props.label}</Button>;
+}    */
+export default function(props) {
+	if(props.label = "Home")
+		return (		
+			<Button variant="outline-dark"><FaHome /></Button>
+	)
+	if(props.label = "Star")
+		return (		
+			<Button variant="outline-dark"><AiOutlineStar /></Button>
+	)
+	if(props.label = "Medal")
+		return (		
+			<Button variant="outline-dark"><FaMedal /></Button>
+	)
+	if(props.label = "Trophy")
+		return (		
+			<Button variant="outline-dark"><AiFillTrophy /></Button>
+	)
 }
