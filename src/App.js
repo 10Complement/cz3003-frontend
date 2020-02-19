1cd07bb4eca0a16045e6bf9895795b5cc0d7b04f
yummilicious <khinyaminthu@hotmail.com>
2020-02-19 19:43:47 +0800

    add profile linkage in app

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ import { NavbarContainer } from "./components/Navbar";
 /*  Master App component that provides routing and encapsulates all components */
 
 const classes = {
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		height: "100vh"
-	},
-	main: {
-		flexGrow: 1
-	}
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh"
+  },
+  main: {
+    flexGrow: 1
+  }
 };
 
 export default function() {
-	return (
-		<div style={classes.container} className="landing">
-			<NavbarContainer title="Quest Game" />
-			<div style={classes.main}>
-				<Router>
-					<Switch>{routes}</Switch>
-				</Router>
-			</div>
-		</div>
-	);
+  return (
+    <div style={classes.container} className="landing">
+      <NavbarContainer title="Quest Game" />
+      <div style={classes.main}>
+        <Router>
+          <Switch>{routes}</Switch>
+        </Router>
+      </div>
+    </div>
+  );
 }
