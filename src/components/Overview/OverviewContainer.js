b91ae25ef2f82d95bf2a22dc9b0b01ec063c1b58
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-02-25 00:00:25 +0800

    Populated OverviewContainer with an example

--- src/components/Overview/OverviewContainer.js
+++ src/components/Overview/OverviewContainer.js
@@ -1,9 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import { Container, Row, Col } from "react-bootstrap";
+import bgImg from "./images/game_background_1.png";
+// Downloaded from https://craftpix.net/freebies/free-horizontal-2d-game-backgrounds/
+
+const styles = {
+	root: {
+		height: "100%",
+		backgroundImage: `url(${bgImg})`,
+		backgroundSize: "cover"
+	},
+	button: {
+		textAlign: "center",
+		padding: "40px 20px"
+	}
+};
 
 export default function() {
 	/* State Declaration */
-	// const { count, setCount } = React.useState(0);
+	// const { count, setCount } = useState(0);
 
 	/* Called only once whenever component is mounted */
 	useEffect(() => {
@@ -11,19 +27,38 @@ export default function() {
 		// Update states
 	}, []);
 
+	const generateWorlds = data => {
+		let worlds = [];
+
+		for (let i = 0; i < data; i++) {
+			worlds.push(
+				<Col style={styles.button} xs={6} md={4}>
+					[Insert IconButton]
+				</Col>
+			);
+		}
+
+		return worlds;
+	};
+
 	return (
-		<>
-			<h1>This is OverviewContainer</h1>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/world/1">World 1</Link>
-					</li>
-					<li>
-						<Link to="/world/1/section/1">World 1 Section 1</Link>
-					</li>
-				</ul>
-			</nav>
-		</>
+		<div style={styles.root}>
+			<Container>
+				<Col>
+					<h1>This is OverviewContainer</h1>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/world/1">World 1</Link>
+							</li>
+							<li>
+								<Link to="/world/1/section/1">World 1 Section 1</Link>
+							</li>
+						</ul>
+					</nav>
+				</Col>
+				<Row>{generateWorlds(6)}</Row>
+			</Container>
+		</div>
 	);
 }
