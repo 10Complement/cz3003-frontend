0921ac8157eee7dc3816c978bba0e3107c626185
Ash <ashwariii97@gmail.com>
2020-02-25 19:28:09 +0800

    icon+text

--- src/App.js
+++ src/App.js
@@ -1,65 +1,10 @@
-/* import React from "react";
-import "./App.css";
-import routes from "./routes/common";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-
-import { NavbarContainer } from "./components/Navbar";   */
-
-/*  Master App component that provides routing and encapsulates all components */
-
-/* const classes = {
-	container: {
-		display: "flex",
-		flexDirection: "column",
-		height: "100vh"
-	},
-	main: {
-		flexGrow: 1
-	}
-}; 
+import React from "react";
+import IconButton from "./components/Common/IconButton"; 
 
 export default function() {
-	return (			
-		<div style={classes.container} className="landing">
-			<NavbarContainer title="Quest Game" />
-			<div style={classes.main}>
-				<Router>
-					<Switch>{routes}</Switch>
-				</Router>
-			</div>			
-		</div> 
-	);
-}
- */
-
-import React from "react";
-import Button from "react-bootstrap/Button";
-import { FaHome } from 'react-icons/fa';
-import { FaMedal } from 'react-icons/fa';
-import { AiFillTrophy } from "react-icons/ai";
-import { AiOutlineStar } from "react-icons/ai"; 
-
-/* export default function(props) {
-	return <Button>{props.label}</Button>;
-}    */
-export default function(props) {
-	// if(props.label = "Home")
-		return (		
-			<Button variant="outline-dark"><FaHome /></Button>
-			/* <Button variant="outline-dark"><AiOutlineStar /></Button>
-		 	<Button variant="outline-dark"><FaMedal /></Button>
-			<Button variant="outline-dark"><AiFillTrophy /></Button> */
+    return (
+      <div>
+        <IconButton message="Home"/>
+      </div>
 	)
-	/* if(props.label = "Star")
-		return (		
-			<Button variant="outline-dark"><AiOutlineStar /></Button>
-	)
-	if(props.label = "Medal")
-		return (		
-			<Button variant="outline-dark"><FaMedal /></Button>
-	)
-	if(props.label = "Trophy")
-		return (		
-			<Button variant="outline-dark"><AiFillTrophy /></Button>
-	) */
-}
+} 
--- src/components/Common/IconButton.js
+++ src/components/Common/IconButton.js
@@ -3,34 +3,61 @@ import Button from "react-bootstrap/Button";
 import { FaHome } from "react-icons/fa";
 import { FaMedal } from "react-icons/fa";
 import { AiFillTrophy } from "react-icons/ai";
-import { AiOutlineStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai"; 
+import { FaGlobeAsia } from "react-icons/fa"; 
 
-/* export default function(props) {
-	return <Button>{props.label}</Button>;
-}    */
+const buttonStyle = {
+	color: 'black',
+	border: '2px',
+	backgroundColor: 'white',
+	fontFamily : 'inherit',
+	height: '50px',
+	width: '200px',
+	borderColor: 'black', 
+	textAlign: 'left',
+	//flex: '1',
+    paddingLeft: '10px',
+  };
+
+const lineStyle = {
+	transform: 'rotate(90deg) translateY(25px) translateX(-32px)',
+	//height: '25px',
+	width: '50px',
+	background: 'grey',
+  };  
+
 export default function(props) {
-	if (props.label === "Home")
-		return (
-			<Button variant="outline-dark">
-				<FaHome />
-			</Button>
+	if (props.message === "Home")
+		return (		
+				<Button style={buttonStyle} variant="outline-dark" size="lg">
+					<FaHome icon={FaHome} size = '30'/>Home
+					<hr style={lineStyle}></hr>
+				</Button>
 		);
-	if (props.label === "Star")
+	if (props.message === "Star")
 		return (
-			<Button variant="outline-dark">
+			<Button variant="outline-dark" size="lg">
 				<AiOutlineStar />
 			</Button>
 		);
-	if (props.label === "Medal")
+	if (props.message === "Medal")
 		return (
-			<Button variant="outline-dark">
+			<Button variant="outline-dark" size="lg">
 				<FaMedal />
 			</Button>
 		);
-	if (props.label === "Trophy")
+	if (props.message === "Trophy" )
 		return (
-			<Button variant="outline-dark">
+			<Button variant="outline-dark" size="lg">
 				<AiFillTrophy />
 			</Button>
 		);
+
+	if (props.message === "World")
+		return (
+			<Button variant="outline-dark" size="lg">
+				<FaGlobeAsia icon={FaGlobeAsia} size = '30'/>   World
+			</Button>
+		);
 }
+  
