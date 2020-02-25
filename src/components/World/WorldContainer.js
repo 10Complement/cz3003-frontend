11d40754dc6287bf6748a6c6b76e7a4cc94e1791
Guang Wei <guangwei.ngoh@gmail.com>
2020-02-25 17:37:34 +0800

    Added section generation to world container

--- src/components/World/WorldContainer.js
+++ src/components/World/WorldContainer.js
@@ -1,8 +1,24 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
+
+import { Container, Row, Col } from "react-bootstrap";
+import bgImg from "../Overview/images/game_background_1.png";
+
+const styles = {
+  root: {
+    height: "100%",
+    backgroundImage: `url(${bgImg})`,
+    backgroundSize: "cover"
+  },
+  button: {
+    textAlign: "center",
+    padding: "40px 20px"
+  }
+};
 
 export default function() {
   const { wID } = useParams();
+  const { pathname } = useLocation();
 
   /* State Declaration */
   // const { count, setCount } = React.useState(0);
@@ -13,14 +29,33 @@ export default function() {
     // Update states
   }, []);
 
+  const generateSections = data => {
+    let sections = [];
+
+    for (let i = 0; i < data; i++) {
+      sections.push(
+        <Col style={styles.button} xs={6} md={4}>
+          <Link to={`${pathname}/section/${i + 1}`}>Section {i + 1}</Link>
+        </Col>
+      );
+    }
+
+    return sections;
+  };
+
   return (
     <>
-      <h1>This is WorldContainer</h1>
-      <p>
-        You are in World ID: {wID}
-        <br />
-        Change the browser URL parameter and see the ID change
-      </p>
+      <div style={styles.root}>
+        <Container>
+          <h1>This is WorldContainer</h1>
+          <p>
+            You are in World ID: {wID}
+            <br />
+            Change the browser URL parameter and see the ID change
+          </p>
+          <Row>{generateSections(6)}</Row>
+        </Container>
+      </div>
     </>
   );
 }
