518bb03805428c272cd3a5d93c72a87dc8ef3d53
Ash <ashwariii97@gmail.com>
2020-02-25 19:28:09 +0800

    icon+text

--- src/App.js
+++ src/App.js
@@ -27,33 +27,12 @@ export default function() {
  */
 
 import React from "react";
-import Button from "react-bootstrap/Button";
-import { FaHome } from 'react-icons/fa';
-import { FaMedal } from 'react-icons/fa';
-import { AiFillTrophy } from "react-icons/ai";
-import { AiOutlineStar } from "react-icons/ai"; 
+import IconButton from "./components/Common/IconButton"; 
 
-/* export default function(props) {
-	return <Button>{props.label}</Button>;
-}    */
-export default function(props) {
-	// if(props.label = "Home")
-		return (		
-			<Button variant="outline-dark"><FaHome /></Button>
-			/* <Button variant="outline-dark"><AiOutlineStar /></Button>
-		 	<Button variant="outline-dark"><FaMedal /></Button>
-			<Button variant="outline-dark"><AiFillTrophy /></Button> */
+export default function() {
+    return (
+      <div>
+        <IconButton message="Home"/>
+      </div>
 	)
-	/* if(props.label = "Star")
-		return (		
-			<Button variant="outline-dark"><AiOutlineStar /></Button>
-	)
-	if(props.label = "Medal")
-		return (		
-			<Button variant="outline-dark"><FaMedal /></Button>
-	)
-	if(props.label = "Trophy")
-		return (		
-			<Button variant="outline-dark"><AiFillTrophy /></Button>
-	) */
-}
+} 
