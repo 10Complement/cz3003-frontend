7f4dfb170197e7235066e4451878063f1037b723
Ash <ashwariii97@gmail.com>
2020-02-26 16:59:46 +0800

    edited typo

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ import Table from './components/Common/Table.js';
 export default function() {
     return (
       <div>
-		<IconButton icon="Star" message="Trophy"/>
+		<IconButton icon="Home" message="Home"/>
       </div>
 	)
 }  
