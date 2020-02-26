ffb6b1f350eaa90c826c27fa9ae5003d3653d4d2
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-02-26 16:17:20 +0800

    Badges now have a fixed size

--- src/components/Common/Badges.js
+++ src/components/Common/Badges.js
@@ -1,29 +1,34 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 import Medal from "./images/medal.svg";
 import Star from "./images/star.svg";
 
-/** TODO:
- * 1. SVG should have a fixed pixel size?
- * 1a. Try mobile view in the OverviewContainer - it doesn't scale properly
- */
+const styles = {
+  badgeContainer: {
+    display: "flex"
+  },
+  badgeItem: {
+    color: "white",
+    margin: "0 10px"
+  },
+  badgeIcon: {
+    width: "35px",
+    margin: "0 10px"
+  }
+};
 
 export default function(props) {
   return (
-    <div style={{ maxWidth: "180px" }}>
-      <Row>
-        <Col md={6}>
-          <Image src={Medal} style={{ width: "50%" }} roundedCircle />
-          <span style={{ color: "white" }}>&nbsp;&nbsp;{props.medals}</span>
-        </Col>
-        <Col md={6}>
-          <Image src={Star} style={{ width: "50%" }} roundedCircle />
-          <span style={{ color: "white" }}>&nbsp;&nbsp;{props.stars}</span>
-        </Col>
-      </Row>
+    <div style={styles.badgeContainer}>
+      <span style={styles.badgeItem}>
+        <Image src={Medal} style={styles.badgeIcon} draggable={false} />
+        {props.medals}
+      </span>
+      <span style={styles.badgeItem}>
+        <Image src={Star} style={styles.badgeIcon} draggable={false} />
+        {props.stars}
+      </span>
     </div>
   );
 }
