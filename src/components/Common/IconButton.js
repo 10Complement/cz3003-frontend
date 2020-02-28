447e19723953688bb1188d2b69d65fbbadd477dd
Ash <ashwariii97@gmail.com>
2020-02-28 11:42:16 +0800

    Edited IconButton with rating stars.

--- src/components/Common/IconButton.js
+++ src/components/Common/IconButton.js
@@ -1,79 +1,32 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import { FaHome } from "react-icons/fa";
-import { FaMedal } from "react-icons/fa";
-import { AiFillTrophy } from "react-icons/ai";
-import { AiOutlineStar } from "react-icons/ai"; 
-import { FaGlobeAsia } from "react-icons/fa"; 
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Rating from 'material-ui-rating'
 
-const buttonStyle = {
-	//color: 'black',
-	border: '5px',
-	//backgroundColor: 'white',
-	fontFamily : 'sans-serif',
-	height: '50px',
-	width: '170px',
-	borderColor: 'black', 
-	textAlign: 'left',
-	//flex: '1',
-    paddingLeft: '10px',
-  };
-
-const lineStyle = {
-	transform: 'rotate(90deg) translateY(30px) translateX(-32px)',
-	//height: '25px',
-	width: '50px',
-	background: 'white',
-  };  
-
+const useStyles = makeStyles(theme => ({
+	button: {
+	  margin: theme.spacing(1),
+	},
+  }));
+  
 export default function(props) {
-	if (props.icon === "Home")
+	const stars = props.stars || 0
+	const classes = useStyles();
 		return (		
-				<Button style={buttonStyle} variant="dark" size="lg">
-					<FaHome icon={FaHome} size = '30' paddingLeft='100px' textAlign= 'left'/>
-					<text style={{paddingLeft:'40px', margin:'0'}}>{props.message}</text>
-					<hr style={lineStyle}></hr>
-				</Button>
-		);
-	if (props.icon === "Star")
-		return (
-			<Button style={buttonStyle} variant="info" size="lg">
-				<AiOutlineStar icon={AiOutlineStar} size = '30' paddingLeft='100px' textAlign= 'left'/>
-				<text style={{paddingLeft:'40px', margin:'0'}}>{props.message}</text>
-				<hr style={lineStyle}></hr>
-			</Button>
-		);
-	if (props.icon === "Medal")
-		return (
-			<Button style={buttonStyle} variant="info" size="lg">
-				<FaMedal icon={FaMedal} size = '30' paddingLeft='100px' textAlign= 'left'/>
-				<text style={{paddingLeft:'40px', margin:'0'}}>{props.message}</text>
-				<hr style={lineStyle}></hr>
-			</Button>
-		);
-	if (props.icon === "Trophy" )
-		return (
-			<Button style={buttonStyle} variant="info" size="lg">
-				<AiFillTrophy icon={AiFillTrophy} size = '30' paddingLeft='100px' textAlign= 'left'/>
-				<text style={{paddingLeft:'40px', margin:'0'}}>{props.message}</text>
-				<hr style={lineStyle}></hr>
-			</Button>
-		);
-
-	if (props.icon === "World")
-		return (
-			<Button style={buttonStyle} variant="info" size="lg">
-				<FaGlobeAsia icon={FaGlobeAsia} size = '30' paddingLeft='100px' textAlign= 'left'/>  
-				<text style={{paddingLeft:'40px', margin:'0'}}>{props.message}</text>
-				<hr style={lineStyle}></hr>
-			</Button>
-		);
-
-	if (props.icon === "")
-		return (
-			<Button style={buttonStyle} variant="info" size="lg">
-				{props.message}
-			</Button>
+			<div align="center"> 
+					<Button
+						variant="contained"
+						color="default"
+						className={classes.button}
+						startIcon={<props.icon/>}
+						size="large">
+					{props.message}
+					</Button>
+				
+					<Box component="fieldset" mb={4} borderColor="transparent">
+							<Rating name="read-only" value= {stars} max='3' readOnly />
+					</Box> 			
+			</div>
 		);
 }
-  
