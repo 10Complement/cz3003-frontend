4593bb605918b2419399a240d4bfcecb5c69e75e
Muhaimin <shutup_95@hotmail.com>
2020-03-04 15:23:39 +0800

    added axios api call for questions

--- src/components/Section/Question.js
+++ src/components/Section/Question.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-export default function() {
-  return (
-    <Card bg="dark" border="dark" text="white">
-      <Card.Header>Mathematics (World)</Card.Header>
-      <Card.Body>
-        <Card.Title>Algebraic Expressions (Section)</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
-          Algebra Addition (Topic)
-        </Card.Subtitle>
-        <Card.Text>Q1) What is a(a+b)?</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+export default function(props) {
+	const { children } = props;
+	return (
+		<Card bg="dark" border="dark" text="white">
+			<Card.Header>Mathematics (World)</Card.Header>
+			<Card.Body>
+				<Card.Title>Algebraic Expressions (Section)</Card.Title>
+				<Card.Subtitle className="mb-2 text-muted">
+					Algebra Addition (Topic)
+				</Card.Subtitle>
+				<Card.Text>{children}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 }
--- src/components/Section/QuestionContainer.js
+++ src/components/Section/QuestionContainer.js
@@ -5,47 +5,87 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import AnswerButton from "./AnswerButton";
 import Question from "./Question";
+import { useParams, useLocation } from "react-router-dom";
+
+const axios = require("axios");
 
 export default function() {
-  /* State Declaration */
-  // const { count, setCount } = React.useState(0);
+	const { wID } = useParams();
+	const { sID } = useParams();
+	const { pathname } = useLocation();
+	/* State Declaration */
+	const [question, setQuestion] = React.useState("");
 
-  /* Called only once whenever component is mounted */
-  useEffect(() => {
-    // console.log(useParams());
-    // Perform API calls
-    // Update states
-  }, []);
+	let all_easy = "",
+		all_med = "",
+		all_hard = "";
 
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col>
-            <Question></Question>
-          </Col>
-        </Row>
-        <br />
-        <Row>
-          <Col>
-            <AnswerButton label="First Answer" isAns={true}></AnswerButton>
-          </Col>
-          <Col>
-            <AnswerButton label="Second Answer" isAns={false}></AnswerButton>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <AnswerButton label="T" isAns={false}></AnswerButton>
-          </Col>
-          <Col>
-            <AnswerButton
-              label="Very very very very very very very very very very very very very very very long Fourth Answer"
-              isAns={false}
-            ></AnswerButton>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+	/* Called only once whenever component is mounted */
+	console.log(useParams());
+	console.log(useLocation());
+	useEffect(() => {
+		// console.log(useParams());
+		// Perform API calls
+		// Update states
+		axios
+			.get(process.env.REACT_APP_API + "/russ/getques/", {
+				params: {
+					worldID: "World-" + wID,
+					sectionID: wID + "-" + sID
+				}
+			})
+			.then(function(response) {
+				all_easy = response["data"]["easy"];
+				all_med = response["data"]["medium"];
+				all_hard = response["data"]["hard"];
+
+				// handle success
+				console.log(response);
+				console.log(Object.keys(all_easy));
+				const easy = all_easy[Object.keys(all_easy)[0]];
+				setQuestion(easy["question"]);
+				console.log(easy);
+			})
+			.catch(function(error) {
+				// handle error
+				console.log(error);
+			})
+			.then(function() {
+				// always executed
+			});
+	}, []);
+
+	function qustionHandler() {}
+
+	return (
+		<>
+			<Container>
+				<Row>
+					<Col>
+						<Question>{question}</Question>
+					</Col>
+				</Row>
+				<br />
+				<Row>
+					<Col>
+						<AnswerButton label="First Answer" isAns={true}></AnswerButton>
+					</Col>
+					<Col>
+						<AnswerButton label="Second Answer" isAns={false}></AnswerButton>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<AnswerButton label="T" isAns={false}></AnswerButton>
+					</Col>
+					<Col>
+						<AnswerButton
+							label="Very very very very very very very very very very very very very very very long Fourth Answer"
+							isAns={false}
+						></AnswerButton>
+					</Col>
+				</Row>
+			</Container>
+		</>
+	);
 }
