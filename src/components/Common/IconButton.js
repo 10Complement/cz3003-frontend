2c388fe3cc40e4de4e72153e489b38f9f9d59209
Guang Wei <guangwei.ngoh@gmail.com>
2020-03-17 19:20:55 +0800

    reordered iconbutton elements, used react-bootstrap button component

--- src/components/Common/IconButton.js
+++ src/components/Common/IconButton.js
@@ -1,8 +1,9 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Rating from "material-ui-rating";
+import Button from "react-bootstrap/Button";
 
 const useStyles = makeStyles(theme => ({
 	button: {
@@ -11,23 +12,27 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function(props) {
-	const stars = props.stars || 0;
+	const stars = props.stars || 2;
 	const classes = useStyles();
 	return (
 		<div align="center">
 			<Button
-				variant="contained"
-				color="default"
-				className={classes.button}
-				startIcon={<props.icon />}
-				size="large"
+				variant="dark"
+				// color="default"
+				// className={classes.button}
+				// size="large"
 			>
-				{props.message}
+				<div>
+					<props.icon />
+					<br />
+					{props.message}
+					<Rating name="read-only" value={stars} max="3" readOnly />
+				</div>
 			</Button>
 
-			<Box component="fieldset" mb={4} borderColor="transparent">
+			{/* <Box component="fieldset" mb={4} borderColor="transparent">
 				<Rating name="read-only" value={stars} max="3" readOnly />
-			</Box>
+			</Box> */}
 		</div>
 	);
 }
--- src/components/Overview/OverviewContainer.js
+++ src/components/Overview/OverviewContainer.js
@@ -1,66 +1,68 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
 
 import { Container, Row, Col } from "react-bootstrap";
 import bgImg from "./images/game_background_1.png";
+import { IconButton } from "../Common";
 // Downloaded from https://craftpix.net/freebies/free-horizontal-2d-game-backgrounds/
 
 const styles = {
-  root: {
-    height: "100%",
-    backgroundImage: `url(${bgImg})`,
-    backgroundSize: "cover",
-    backgroundAttachment: "fixed"
-  },
-  button: {
-    textAlign: "center",
-    padding: "40px 20px"
-  }
+	root: {
+		height: "100%",
+		backgroundImage: `url(${bgImg})`,
+		backgroundSize: "cover",
+		backgroundAttachment: "fixed"
+	},
+	button: {
+		textAlign: "center",
+		padding: "40px 20px"
+	}
 };
 
 export default function() {
-  /* State Declaration */
-  // const { count, setCount } = useState(0);
+	/* State Declaration */
+	// const { count, setCount } = useState(0);
 
-  /* Called only once whenever component is mounted */
-  useEffect(() => {
-    // Perform API calls
-    // Update states
-  }, []);
+	/* Called only once whenever component is mounted */
+	useEffect(() => {
+		// Perform API calls
+		// Update states
+	}, []);
 
-  const generateWorlds = data => {
-    let worlds = [];
+	const generateWorlds = data => {
+		let worlds = [];
 
-    for (let i = 0; i < data; i++) {
-      worlds.push(
-        <Col key={i} style={styles.button} xs={6} md={4}>
-          <Link key={i} to={`/world/${i + 1}`}>
-            World {i + 1}
-          </Link>
-        </Col>
-      );
-    }
+		for (let i = 0; i < data; i++) {
+			worlds.push(
+				<Col key={i} style={styles.button} xs={6} md={4}>
+					<Link key={i} to={`/world/${i + 1}`}>
+						World {i + 1}
+					</Link>
+				</Col>
+			);
+		}
 
-    return worlds;
-  };
+		return worlds;
+	};
 
-  return (
-    <div style={styles.root}>
-      <Container>
-        <h1>This is OverviewContainer</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/world/1">World 1</Link>
-            </li>
-            <li>
-              <Link to="/world/1/section/1">World 1 Section 1</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Row>{generateWorlds(6)}</Row>
-      </Container>
-    </div>
-  );
+	return (
+		<div style={styles.root}>
+			<Container>
+				<h1>This is OverviewContainer</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/world/1">World 1</Link>
+						</li>
+						<li>
+							<Link to="/world/1/section/1">World 1 Section 1</Link>
+						</li>
+					</ul>
+				</nav>
+				<IconButton icon={AcUnitIcon} message="Button"></IconButton>
+				<Row>{generateWorlds(6)}</Row>
+			</Container>
+		</div>
+	);
 }
