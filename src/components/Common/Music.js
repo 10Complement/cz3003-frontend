cb9ce9aa36cbf33ec9990bff9597a4b904480774
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-03-28 17:05:47 +0800

    Music on/off button

--- src/components/Common/Music.js
+++ src/components/Common/Music.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Sound from "react-sound";
+import Image from "react-bootstrap/Image";
+
+import Speaker from "./images/speaker.svg";
+import Mute from "./images/mute.svg";
+
+import bgMusic from "../Common/sound/Our-Mountain_v003_Looping.mp3";
+//import bgMusic from "../Common/sound/Lost-Jungle_Looping.mp3";
+//Source: https://soundimage.org/fantasywonder/
+
+const styles = {
+	icon: {
+		width: "35px",
+        margin: "0 10px",
+	  }
+}
+
 
 export default function(props) {
 
-    var playStatus;
+    const [musicState, setMusicState] = useState(Sound.status.PLAYING);
+    const [iconState, setIconState] = useState(Speaker);
 
-    if (props.playStatus == "playing") {
-        playStatus = Sound.status.PLAYING;
-    } else if (props.playStatus == "paused") {
-        playStatus = Sound.status.PAUSED;
-    } else {
-        playStatus = Sound.status.STOPPED;
+    function switchState() {
+
+        if (musicState === Sound.status.PAUSED) { //Does not trigger when I pause/play the music using my headset buttons
+            setMusicState(Sound.status.PLAYING);
+            setIconState(Speaker);
+        } else {
+            setMusicState(Sound.status.PAUSED);
+            setIconState(Mute);
+        }
+        
     }
 
-    return <Sound 
-        url= {props.url}
-        playStatus={playStatus} 
-        loop={true}
-    />;
+    return <div>
+        <Image src={iconState} style={styles.icon} draggable={false} onClick={() => switchState()}/>
+        <Sound 
+            url= {bgMusic}
+            playStatus={musicState} 
+            loop={true}
+        />
+    </div>;
 }
 
 //Issue with chrome browse: when using the refresh button of the browser, the music won't autoplay anymore, because of some stupid webbrowser policies preventing sound to autoplay
--- src/components/Navbar/NavbarContainer.js
+++ src/components/Navbar/NavbarContainer.js
@@ -6,21 +6,17 @@ import { Music } from "../Common";
 import { UserContext } from "../../contexts/UserContext";
 import { Badges } from "../Common";
 
-import bgMusic from "../Common/sound/Our-Mountain_v003_Looping.mp3";
-//import bgMusic2 from "../Common/sound/Lost-Jungle_Looping.mp3";
-//Source: https://soundimage.org/fantasywonder/
-
 export default function() {
 	const { student } = useContext(UserContext);
 	const { name, medals = 3, stars = 12 } = student;
 
 	return (
 		<Navbar expand="lg" variant="dark" bg="dark">
-			<Music url= {bgMusic} playStatus="playing" />
 			<Container>
 				<Navbar.Brand as={Link} to="/">
 					{name ? `Welcome, ${name}!` : "SDLC Quest"}
 				</Navbar.Brand>
+				<Music />
 				<Badges medals={medals} stars={stars} />
 			</Container>
 		</Navbar>
