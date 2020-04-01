c84d01b653def60bb6d8bfd4fd876b09af5361b2
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-01 23:25:01 +0800

    Commented out unused imports

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
