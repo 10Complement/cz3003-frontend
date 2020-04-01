25178c5554ac6d8c7a656a95d9a7305384a14546
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-04-01 18:58:57 +0800

    Separate components

--- src/components/Arena/ArenaContainer.js
+++ src/components/Arena/ArenaContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import Container from "react-bootstrap/Container";
 
+import TabPanel from "./TabPanel";
 import QuestionPanel from "./QuestionPanel";
 import AssignmentPanel from "./AssignmentPanel";
 
@@ -15,29 +13,6 @@ const styles = {
 		paddingTop: "20px",
 		paddingBottom: "20px"
 	}
-};
-
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`full-width-tabpanel-${index}`}
-            aria-labelledby={`full-width-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={3}>{children}</Box>}
-        </Typography>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
 };
 
 function a11yProps(index) {
