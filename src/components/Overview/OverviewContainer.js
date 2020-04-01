ddb9ed8834720a4b81918d8efb7c5939b2638ec4
Guang Wei <guangwei.ngoh@gmail.com>
2020-04-01 19:23:17 +0800

    Added parallax background to overview

--- src/components/Overview/OverviewContainer.js
+++ src/components/Overview/OverviewContainer.js
@@ -1,9 +1,17 @@
 import React, { useEffect } from "react";
+import Parallax from "parallax-js";
 import { Link } from "react-router-dom";
 import PublicIcon from "@material-ui/icons/Public";
 
 import { Container, Row, Col } from "react-bootstrap";
 import bgImg from "./images/game_background_1.png";
+import sky from "./images/sky.png";
+import rocks1 from "./images/rocks_1.png";
+import rocks2 from "./images/rocks_2.png";
+import clouds1 from "./images/clouds_1.png";
+import clouds2 from "./images/clouds_2.png";
+import clouds3 from "./images/clouds_3.png";
+import clouds4 from "./images/clouds_4.png";
 import { IconButton } from "../Common";
 // Downloaded from https://craftpix.net/freebies/free-horizontal-2d-game-backgrounds/
 
@@ -11,14 +19,26 @@ const axios = require("axios");
 
 const styles = {
 	root: {
-		height: "100%",
-		backgroundImage: `url(${bgImg})`,
-		backgroundSize: "cover",
-		backgroundAttachment: "fixed"
+		height: "100%"
+		// backgroundImage: `url(${bgImg})`,
+		// backgroundSize: "cover",
+		// backgroundAttachment: "fixed"
 	},
 	button: {
 		textAlign: "center",
 		padding: "40px 20px"
+	},
+	parallax: {
+		maxWidth: "100%",
+		position: "fixed",
+		zIndex: "-10"
+	},
+	parallaxchild: {
+		// objectFit: "cover"
+	},
+	parallaximg: {
+		maxWidth: "100%",
+		maxHeight: "100%"
 	}
 };
 
@@ -32,6 +52,10 @@ export default function() {
 	useEffect(() => {
 		// Perform API calls
 		// Update states
+
+		var scene = document.getElementById("scene");
+		var parallaxInstance = new Parallax(scene);
+
 		const worlds = [
 			"World-1",
 			"World-2",
@@ -75,6 +99,64 @@ export default function() {
 
 	return (
 		<div style={styles.root}>
+			<div id="scene" style={styles.parallax}>
+				<div data-depth="0.0" style={styles.parallaxchild}>
+					<img
+						src={sky}
+						alt="sky"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.1" style={styles.parallaxchild}>
+					<img
+						src={rocks1}
+						alt="rocks1"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.2" style={styles.parallaxchild}>
+					<img
+						src={rocks2}
+						alt="rocks2"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.3" style={styles.parallaxchild}>
+					<img
+						src={clouds1}
+						alt="clouds1"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.4" style={styles.parallaxchild}>
+					<img
+						src={clouds2}
+						alt="clouds2"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.5" style={styles.parallaxchild}>
+					<img
+						src={clouds3}
+						alt="clouds3"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+				<div data-depth="0.6" style={styles.parallaxchild}>
+					<img
+						src={clouds4}
+						alt="clouds4"
+						draggable={false}
+						style={styles.parallaximg}
+					/>
+				</div>
+			</div>
 			<Container>
 				<h1>This is OverviewContainer</h1>
 				<nav>
@@ -88,7 +170,26 @@ export default function() {
 					</ul>
 				</nav>
 				<Row>{worldButtons}</Row>
+
+				{/* <ul id="scene">
+					<li className="layer" dataDepth="0.00">
+						Testing 1
+						<img src="./images/clouds1.png" />
+					</li>
+					<li className="layer" dataDepth="0.50">
+						Testing 2
+						<img src="./images/clouds2.png" />
+					</li>
+					<li className="layer" dataDepth="1.00">
+						Testing 3
+						<img src="./images/clouds3.png" />
+					</li>
+				</ul> */}
 			</Container>
+			{/* <script type="text/javascript">
+				var scene = document.getElementById("scene"); var parallaxInstance = new
+				Parallax(scene);
+			</script> */}
 		</div>
 	);
 }
