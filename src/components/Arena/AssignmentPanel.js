5dfa2352a475eb8dd50437c8782768865dbf6bcd
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-04-02 15:56:37 +0800

    Separate table component

--- src/components/Arena/AssignmentPanel.js
+++ src/components/Arena/AssignmentPanel.js
@@ -1,76 +1,35 @@
-import React, { useEffect, useState, forwardRef } from "react";
-import MaterialTable from "material-table";
+import React from "react";
 import axios from "axios";
 
-//Icons for table
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import {TableStructure} from "../Common";
+
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
-const tableIcons = {
-	Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-	Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-	Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-	Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-	DetailPanel: forwardRef((props, ref) => (
-		<ChevronRight {...props} ref={ref} />
-	)),
-	Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-	Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-	Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-	FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-	LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-	NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-	PreviousPage: forwardRef((props, ref) => (
-		<ChevronLeft {...props} ref={ref} />
-	)),
-	ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-	Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-	SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-	ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-	ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
-const data = [ //Replace with fetched data
-    {title: "Multiplication", status: "Submitted", teacher: "Algebra teacher"},
-    {title: "Algebra", status: "Not attempted", teacher: "Algebra teacher"},
-    {title: "Geometry", status: "Submitted", teacher: "Geometry teacher"}
-]
-
 export default function() {
-    return <MaterialTable
-        icons={tableIcons}
-        title="Assignments"
-        columns={[
-            {title: 'Title', field: 'title'},
-            {title: 'Status', field: 'status'}, //Submitted or not attempted
-            {title: 'Teacher', field: 'teacher'} //Teacher who gave out the assignment and is responsible for it
-        ]}
-        data={data}
-        actions={[
-            {
-                icon: ArrowForwardIcon,
-                tooltip: "Attempt question",
-                onClick: (event, rowData) => {} //Function to load assignment (iff not already submitted!)
-            }
-        ]}
-        options={{
-            filtering: true, //Not sure we want this
-            sorting: true,
-            actionsColumnIndex: -1
-        }}
-    />;
+
+    const title = "Assignments"
+    const columns = [
+        {title: 'Title', field: 'title'},
+        {title: 'Status', field: 'status'}, //Submitted or not attempted
+        {title: 'Teacher', field: 'teacher'} //Teacher who gave out the assignment and is responsible for it
+    ]
+    const data = [ //Replace with fetched data
+        {title: "Multiplication", status: "Submitted", teacher: "Algebra teacher"},
+        {title: "Algebra", status: "Not attempted", teacher: "Algebra teacher"},
+        {title: "Geometry", status: "Submitted", teacher: "Geometry teacher"}
+    ]
+    const actions = [
+        {
+            icon: ArrowForwardIcon,
+            tooltip: "Attempt question",
+            onClick: (event, rowData) => {} //Function to load assignment (iff not already submitted!)
+        }
+    ]
+    const options = {
+        filtering: true, //Not sure we want this
+        sorting: true,
+        actionsColumnIndex: -1
+    }
+
+    return <TableStructure title={title} columns={columns} data={data} actions={actions} options={options}/>;
 }
--- src/components/Leaderboard/LeaderboardContainer.js
+++ src/components/Leaderboard/LeaderboardContainer.js
@@ -1,48 +1,10 @@
-import React, { useEffect, useState, forwardRef } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
-import MaterialTable from "material-table";
 import axios from "axios";
 
-//Icons for table
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import {TableStructure} from "../Common";
 
-const tableIcons = {
-	Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-	Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-	Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-	Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-	DetailPanel: forwardRef((props, ref) => (
-		<ChevronRight {...props} ref={ref} />
-	)),
-	Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-	Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-	Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-	FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-	LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-	NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-	PreviousPage: forwardRef((props, ref) => (
-		<ChevronLeft {...props} ref={ref} />
-	)),
-	ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-	Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-	SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-	ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-	ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
+import SaveAlt from "@material-ui/icons/SaveAlt";
 
 const styles = {
 	leaderboardContainer: {
@@ -111,37 +73,35 @@ export default function() {
 		})
 	}
 
+	const title = "Leaderboard"
+	const columns = [
+		{title: 'Avatar', field: 'avatar', filtering: false, render: rowData => 
+			<img src={rowData.avatar} alt="Avatar" style={styles.avatar}/>},
+		{title: 'Name', field: 'name'},
+		{title: 'Student ID', field: 'studentID'},
+		{title: 'Class', field: 'class'},
+		{title: 'Current Progress', field: 'progress'},
+		{title: 'Stars', field: 'stars'},
+		{title: 'Medals', field: 'medals'}
+	]
+	const actions = [
+		{
+			icon: SaveAlt,
+			tooltip: "Generate report",
+			onClick: (event, rowData) =>
+				generateReport(rowData.studentID, rowData.name, rowData.class)
+		}
+	]
+	const options = {
+		exportButton: true,
+		filtering: true,
+		sorting: true,
+		actionsColumnIndex: -1
+	}
+
 	return (
 		<Container size="sm" style={styles.leaderboardContainer}>
-			<MaterialTable
-				icons={tableIcons}
-				title="Leaderboard"
-				columns={[
-					{title: 'Avatar', field: 'avatar', filtering: false, render: rowData => 
-						<img src={rowData.avatar} alt="Avatar" style={styles.avatar}/>},
-					{title: 'Name', field: 'name'},
-					{title: 'Student ID', field: 'studentID'},
-					{title: 'Class', field: 'class'},
-					{title: 'Current Progress', field: 'progress'},
-					{title: 'Stars', field: 'stars'},
-					{title: 'Medals', field: 'medals'}
-				]}
-				data={dataSet}
-				actions={[
-					{
-						icon: SaveAlt,
-						tooltip: "Generate report",
-						onClick: (event, rowData) =>
-							generateReport(rowData.studentID, rowData.name, rowData.class)
-					}
-				]}
-				options={{
-					exportButton: true,
-					filtering: true,
-					sorting: true,
-					actionsColumnIndex: -1
-				}}
-			/>
+			<TableStructure title={title} columns={columns} data={dataSet} actions={actions} options={options}/>
 		</Container>
 	);
 }
