b4d121311726cc3d3820adc153608adeda8cf880
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-05 01:18:52 +0800

    setCorrect is now only called once

--- src/components/Section/QuestionAnswer.js
+++ src/components/Section/QuestionAnswer.js
@@ -5,33 +5,30 @@ import Col from "react-bootstrap/Col";
 import AnswerButton from "./AnswerButton";
 import Question from "./Question";
 
-const sampleQnSet = {
-	id: "randomstring",
-	answer: 0,
-	question: "This is a sample question",
-	options: ["Correct answer", "Wrong answer"]
-};
+// const sampleQnSet = {
+// 	id: "randomstring",
+// 	answer: 0,
+// 	question: "This is a sample question",
+// 	options: ["Correct answer", "Wrong answer"]
+// };
 
 export default function(props) {
-	const {
-		qnSet = sampleQnSet,
-		title = "Loading...",
-		subtitle,
-		onFirstResponse
-	} = props;
+	const { qnSet, title, subtitle, onFirstResponse } = props;
 	const [correctFirstTry, setCorrectFirstTry] = useState("NIL");
 	const [allOptionButtons, setAllOptionButtons] = useState([]);
 
 	/* Renders all option buttons */
 	useEffect(() => {
+		if (!qnSet) return;
+
 		const { id, answer, options } = qnSet;
 
 		const handleAnswerClick = (id, isAns) => {
-			// Only setCorrect once!
+			// Only call setCorrect once!
 			// User is classified as correct/wrong on the first click.
 			isAns === true
-				? setCorrectFirstTry(c => (c === "NIL") === true)
-				: setCorrectFirstTry(c => (c === "NIL") === false);
+				? setCorrectFirstTry(c => c === "NIL" && true)
+				: setCorrectFirstTry(c => c === "NIL" && false);
 		};
 
 		const all_options = options.map((option, i) => (
@@ -63,7 +60,7 @@ export default function(props) {
 			<Row>
 				<Col>
 					<Question title={title} subtitle={subtitle}>
-						{qnSet.question}
+						{qnSet && qnSet.question}
 					</Question>
 				</Col>
 			</Row>
