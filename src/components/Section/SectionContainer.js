67cae06e94fb18de4fe78f35d180bd57b006b215
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-05 02:39:22 +0800

    Implemented question manager with useReducer

--- src/components/Section/SectionContainer.js
+++ src/components/Section/SectionContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
@@ -21,11 +21,56 @@ const styles = {
 	}
 };
 
+const levels = ["easy", "medium", "hard"];
+
+const initQn = {
+	difficulty: 0,
+	history: {},
+	current: {
+		id: "randomstring",
+		answer: 0,
+		question: "Loading...",
+		options: []
+	}
+};
+
+const qnReducer = (state, action) => {
+	let { history, current, difficulty, bank } = state;
+	switch (action.type) {
+		case "INIT":
+			bank = action.data;
+			break;
+		case "NEXT_QN":
+			/* 1. Increase level only if user got question correct on first try */
+			if (history[current.id] === true) {
+				if (difficulty < levels.length - 2) difficulty++;
+			}
+
+			/* 2. Update current */
+			const difficultyText = levels[difficulty];
+			const newKey = randomUniqueKey(bank[difficultyText], history);
+
+			current = { ...bank[difficultyText][newKey], id: newKey };
+
+			break;
+		case "HISTORY":
+			let { id, correctFirstTry } = action;
+
+			history[id] = correctFirstTry;
+			break;
+		default:
+			console.error("Invalid action type passed into qnBankReducer");
+	}
+
+	return { history, current, difficulty, bank };
+};
+
 export default function() {
 	const { wID, sID } = useParams();
 
 	/* State Declaration */
-	const [qnBank, setQnBank] = useState();
+	// const [qnBank, setQnBank] = useState();
+	const [qn, dispatchQn] = useReducer(qnReducer, initQn);
 
 	/* Called only once whenever component is mounted */
 	useEffect(() => {
@@ -37,12 +82,21 @@ export default function() {
 				}
 			})
 			.then(response => {
-				setQnBank(response.data);
+				dispatchQn({ type: "INIT", data: response.data });
+				dispatchQn({ type: "NEXT_QN" });
 			})
 			.catch(error => {
-				console.log(error);
+				console.error(error);
 			});
 	}, [wID, sID]);
+
+	const recordHistory = (id, correctFirstTry) => {
+		dispatchQn({
+			type: "HISTORY",
+			id: id,
+			correctFirstTry: correctFirstTry
+		});
+	};
 
 	return (
 		<>
@@ -57,7 +111,15 @@ export default function() {
 							<Learning />
 						</Col>
 						<Col>
-							<QuestionAnswer qnBank={qnBank} />
+							<QuestionAnswer
+								title={`Question x of 3`}
+								subtitle={
+									levels[qn.difficulty].charAt(0).toUpperCase() +
+									levels[qn.difficulty].substring(1)
+								}
+								qnSet={qn.current}
+								onFirstResponse={recordHistory}
+							/>
 						</Col>
 					</Row>
 				</Container>
@@ -65,3 +127,15 @@ export default function() {
 		</>
 	);
 }
+
+const randomUniqueKey = (questions, history) => {
+	const qnKeys = Object.keys(questions);
+	const histKeys = Object.keys(history);
+	let i = 0;
+
+	do {
+		i = Math.floor(Math.random() * (qnKeys.length - 1));
+	} while (histKeys[qnKeys[i]]);
+
+	return qnKeys[i];
+};
