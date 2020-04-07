e40bca4dcc255a0cf7fda7624b008967a0ef0b34
Yamin <khinyaminthu@hotmail.com>
2020-04-07 18:54:03 +0800

    added background image

--- src/components/Profile/ProfileContainer.js
+++ src/components/Profile/ProfileContainer.js
@@ -6,17 +6,29 @@ import { Badges } from "../Common";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import axios from "axios";
 import MaterialTable from "material-table";
-
-const imagesize = {
-	width: "200px"
-};
+import bgImg from "../Overview/images/game_background_1.png";
 
 const styles = {
+	root: {
+		height: "100%",
+		backgroundImage: `url(${bgImg})`,
+		backgroundSize: "cover",
+		backgroundAttachment: "fixed"
+	},
 	card: {
-		width: "750px"
+		width: "1000px"
+	},
+	sectionStarContainer: {
+		width: "530px",
+		paddingTop: "20px",
+		paddingBottom: "20px"
 	}
+};
+const imagesize = {
+	width: "200px"
 };
 
 export default function(props) {
@@ -61,46 +73,50 @@ export default function(props) {
 	};
 
 	return (
-		<Card
-			bg="dark"
-			border="dark"
-			text="white"
-			className="m-4 p-4"
-			style={styles.card}
-		>
-			<Row>
-				<Col className="d-flex flex-column justify-content-center">
-					<Avatar
-						className="mb-4 w-100 d-flex justify-content-center my-2"
-						size={imagesize}
-					></Avatar>
-					{
+		<div style={styles.root} className="d-flex align-items-center">
+			<Card
+				bg="dark"
+				border="dark"
+				text="white"
+				className="m-4 p-4"
+				style={styles.card}
+			>
+				<Row>
+					<Col className="d-flex flex-column justify-content-center">
+						<Avatar
+							className="mb-4 w-100 d-flex justify-content-center my-2"
+							size={imagesize}
+						></Avatar>
+						{
+							<div class="w-100 d-flex justify-content-center my-2">
+								{playerName}
+							</div>
+						}
+						<div class="w-100 d-flex justify-content-center my-2">{matric}</div>
 						<div class="w-100 d-flex justify-content-center my-2">
-							{playerName}
+							<Badges stars={totalNumberStars} medals={10} />
 						</div>
-					}
-					<div class="w-100 d-flex justify-content-center my-2">{matric}</div>
-					<div class="w-100 d-flex justify-content-center my-2">
-						<Badges stars={totalNumberStars} medals={10} />
-					</div>
 
-					<Button className="w-30 mx-auto" variant="dark">
-						edit
-					</Button>
-				</Col>
-				<Col className="d-flex flex-column align-items-center justify-content-center">
-					<Row>
-						<MaterialTable
-							title="Your progress"
-							columns={[
-								{ title: "Section", field: "section" },
-								{ title: "Stars", field: "stars" }
-							]}
-							data={dataSet}
-						/>
-					</Row>
-				</Col>
-			</Row>
-		</Card>
+						<Button className="w-30 mx-auto" variant="dark">
+							edit
+						</Button>
+					</Col>
+					<Col className="d-flex flex-column align-items-center justify-content-center">
+						<Row>
+							<Container size="sm" style={styles.sectionStarContainer}>
+								<MaterialTable
+									title="Your Progress"
+									columns={[
+										{ title: "Section", field: "section" },
+										{ title: "Stars", field: "stars" }
+									]}
+									data={dataSet}
+								/>
+							</Container>
+						</Row>
+					</Col>
+				</Row>
+			</Card>
+		</div>
 	);
 }
