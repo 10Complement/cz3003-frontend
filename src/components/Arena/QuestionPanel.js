7f908b345652a11f7a1938cd09ab6d754df37c3f
Iris Kremer <iris.kremer.001@student.uni.lu>
2020-04-08 18:25:12 +0800

    Display own questions

--- src/components/Arena/QuestionPanel.js
+++ src/components/Arena/QuestionPanel.js
@@ -23,18 +23,19 @@ export default function() {
     const history = useHistory();
     const student = useContext(UserContext);
     const matric = student.student.matric;
+
+    //Arena questions table data
     const [questions, setQuestions] = useState([]);
 
     useEffect(() => {
-		fetchQuestions();
+		fetchQuestionsArena();
 	}, []);
 
-	const fetchQuestions = () => {
+	const fetchQuestionsArena = () => {
 		axios
 			.get(process.env.REACT_APP_API + "/russ/GetArenaQuestions")
 			.then(res => {
                 const d = res.data;
-                console.log(d);
                 var questionsData = Object.keys(d)
                     .filter(key => { return d[key].creator !== matric })
                     .map(key => {
@@ -43,35 +44,95 @@ export default function() {
                     });
 				setQuestions(questionsData);
 			});
-	};
+    };
 
-    const title = "Arena questions"
-    const columns = [
+    const titleArena = "Arena questions"
+    const columnsArena = [
         {title: 'Question', field: 'question'},
         {title: 'Creator', field: 'creator'},
         {title: 'Total attempts', field: 'totalAttempts'}, //Number of people who attempted this question
         {title: 'Status', field: 'status'} //Attempted or not attempted
     ]
-    const actions = [
+    const actionsArena = [
         {
             icon: ArrowForwardIcon,
             tooltip: "Attempt question",
             onClick: (event, rowData) => {history.push("/arena/question/" + rowData.id)}
         }
     ]
-    const options = {
+    const optionsArena = {
         filtering: true,
         sorting: true,
         actionsColumnIndex: -1
+    }
+
+    //Own questions table data
+    const [questionsOwn, setQuestionsOwn] = useState([]);
+
+	const fetchQuestionsOwn = () => {
+		axios
+			.get(process.env.REACT_APP_API + "/russ/GetArenaQuestions")
+			.then(res => {
+                const d = res.data;
+                var questionsData = Object.keys(d)
+                    .filter(key => { return d[key].creator === matric })
+                    .map(key => {
+                        const status = (d[key].players && Object.keys(d[key].players).includes(matric)) ? "Attempted" : "Not attempted";
+                        return {id: key, question: d[key].question, totalAttempts: d[key].attempts};
+                    });
+				setQuestions(questionsData);
+			});
+    };
+
+    const titleOwn = "Own questions"
+    const columnsOwn = [
+        {title: 'Question', field: 'question'},
+        {title: 'Total attempts', field: 'totalAttempts'}, //Number of people who attempted this question
+    ]
+    const actionsOwn = []
+    const optionsOwn = {
+        filtering: true,
+        sorting: true,
+    }
+
+    //Page data
+    const [buttonMsg, setButtonMsg] = useState("Own questions");
+    const [tableData, setTableData] = useState({
+        title: titleArena,
+        columns: columnsArena,
+        actions: actionsArena,
+        options: optionsArena
+    });
+
+    const switchTable = () => {
+        if (buttonMsg === "Own questions") {
+            fetchQuestionsOwn();
+            setButtonMsg("Arena questions");
+            setTableData({
+                title: titleOwn,
+                columns: columnsOwn,
+                actions: actionsOwn,
+                options: optionsOwn
+            })
+        } else { //buttonMsg === "Arena questions"
+            fetchQuestionsArena();
+            setButtonMsg("Own questions");
+            setTableData({
+                title: titleArena,
+                columns: columnsArena,
+                actions: actionsArena,
+                options: optionsArena
+            })
+        }
     }
 
     return <div style={styles.root}>
             <Container>
                 <Row style={styles.row}>
                     <Col><ArenaButton message="New question"/></Col>
-                    <Col><ArenaButton message="Own questions"/></Col>
+                    <Col><div onClick={switchTable}><ArenaButton message={buttonMsg}/></div></Col>
                 </Row>
             </Container>
-            <TableStructure title={title} columns={columns} data={questions} actions={actions} options={options}/>
+            <TableStructure title={tableData.title} columns={tableData.columns} data={questions} actions={tableData.actions} options={tableData.options}/>
         </div>;
 }
