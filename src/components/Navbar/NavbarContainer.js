3fce83ce9c18ce2f5f045708e3b8003b270c943e
Ash <ashwariii97@gmail.com>
2020-04-08 18:55:06 +0800

    Display of login / logout icon.

--- src/components/Navbar/NavbarContainer.js
+++ src/components/Navbar/NavbarContainer.js
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom";
 import { Container, Navbar } from "react-bootstrap";
 import { Music, Badges } from "../Common";
 import Logout from "./images/logout.svg";
+import Login from "./images/login.svg";
+import { useHistory } from "react-router-dom";
 
 import { UserContext } from "../../contexts/UserContext";
 
 const styles = {
 	logoutSpan: {
 		color: "white",
-		cursor: "pointer",
+		cursor: "pointer"
 	},
 	logoutIcon: { width: "35px" },
+	badgeContainer: {
+		display: "flex",
+		textAlign: "center"
+	}
 };
 
-export default function () {
+export default function() {
+	const history = useHistory();
 	const { student, setStudent } = useContext(UserContext);
-	const { matric, medals = "u", stars = "u" } = student;
-
+	const { matric, medals, stars } = student;
 	const clearSess = () => {
 		const s = {
 			matric: undefined,
@@ -26,23 +32,35 @@ export default function () {
 			current_progress: undefined,
 			avatar_url: undefined,
 			stars: undefined,
-			medals: undefined,
+			medals: undefined
 		};
 		setStudent(s);
 		alert("You've been succefully logged out!");
+		history.push("/login");
 	};
 
 	return (
 		<Navbar expand="lg" variant="dark" bg="dark" fixed="top">
-			<Container>
-				<Navbar.Brand as={Link} to={matric ? "/" : "/login"}>
-					{matric ? `Welcome, ${matric}!` : "Please login (debug)"}
+			<Container style={styles.badgeContainer}>
+				<Navbar.Brand as={Link} to={"/login"}>
+					{matric ? (
+						`Welcome, ${matric}!`
+					) : (
+						<img src={Login} style={styles.logoutIcon} draggable={false}></img>
+					)}
 				</Navbar.Brand>
 				<Music />
 				<Badges medals={medals} stars={stars} />
-				<span style={styles.logoutSpan} onClick={clearSess}>
-					<img src={Logout} style={styles.logoutIcon} draggable={false} />
-				</span>
+				<Navbar.Brand style={styles.logoutSpan}>
+					{matric ? (
+						<img
+							src={Logout}
+							style={styles.logoutIcon}
+							onClick={clearSess}
+							draggable={false}
+						></img>
+					) : null}
+				</Navbar.Brand>
 			</Container>
 		</Navbar>
 	);
