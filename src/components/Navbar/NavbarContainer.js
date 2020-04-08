f5c2998adf41abe1845f54c39bf104c720e3ab12
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-08 15:56:56 +0800

    Login prompt for debugging user session

--- src/components/Navbar/NavbarContainer.js
+++ src/components/Navbar/NavbarContainer.js
@@ -8,13 +8,13 @@ import { Badges } from "../Common";
 
 export default function () {
 	const { student } = useContext(UserContext);
-	const { matric, medals = 3, stars = 12 } = student;
+	const { matric, medals = "u", stars = "u" } = student;
 
 	return (
 		<Navbar expand="lg" variant="dark" bg="dark" fixed="top">
 			<Container>
-				<Navbar.Brand as={Link} to="/">
-					{matric ? `Welcome, ${matric}!` : "SDLC Quest"}
+				<Navbar.Brand as={Link} to={matric ? "/" : "/login"}>
+					{matric ? `Welcome, ${matric}!` : "Please login (debug)"}
 				</Navbar.Brand>
 				<Music />
 				<Badges medals={medals} stars={stars} />
