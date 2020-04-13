9f315a08b834dae8f52e793072936e9dace8c476
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-14 01:20:53 +0800

    Clean up warning messages

--- src/components/Arena/AssignmentContainer.js
+++ src/components/Arena/AssignmentContainer.js
@@ -1,30 +1,29 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
 import { QuestionAnswer } from "../Section";
-import { UserContext } from "../../contexts/UserContext";
+// import { UserContext } from "../../contexts/UserContext";
 import bgImg from "../Overview/images/game_background_1.png";
 
 const styles = {
-    root: {
+	root: {
 		height: "100%",
 		width: "100%",
 		backgroundImage: `url(${bgImg})`,
 		backgroundSize: "cover",
-		backgroundAttachment: "fixed"
+		backgroundAttachment: "fixed",
 	},
 	container: {
 		paddingTop: "20px",
-		paddingBottom: "20px"
-	}
+		paddingBottom: "20px",
+	},
 };
 
-export default function() {
-
+export default function () {
 	const { aID } = useParams();
 	const history = useHistory();
-	const student = useContext(UserContext);
+	// const student = useContext(UserContext);
 	const [questionSet, setQuestion] = useState({
 		id: "",
 		answer: 0,
@@ -32,60 +31,68 @@ export default function() {
 		options: [],
 	});
 	const [title, setTitle] = useState("Loading...");
-    const [subtitle, setSubtitle] = useState("");
-    const [assignment, setAssignment] = useState({
-        title: "Loading...",
-        teacher: "",
-        players: [],
-        question: []
-    });
-    var nextIndex = 0;
-    var score = 0;
+	const [subtitle, setSubtitle] = useState("");
+	const [assignment, setAssignment] = useState({
+		title: "Loading...",
+		teacher: "",
+		players: [],
+		question: [],
+	});
+	var nextIndex = 0;
+	var score = 0;
 
-    useEffect(() => {
+	useEffect(() => {
 		axios
 			.get(process.env.REACT_APP_API + "" + aID) //Missing url
-			.then(res => {
+			.then((res) => {
 				const data = res.data;
-                setAssignment(data);
-                setTitle(data.title);
-                setSubtitle("By teacher " + data.teacher);
-                nextQuestion();
+				setAssignment(data);
+				setTitle(data.title);
+				setSubtitle("By teacher " + data.teacher);
+				nextQuestion();
 			});
-    }, [aID]);
-    
-    const nextQuestion = () => {
-        const qList = assignment.question;
-        if (nextIndex < qList.length()) {
-            const q = qList[nextIndex];
-            setQuestion({
-                id: "question" + nextIndex,
-                answer: q.answer,
-                question: q.question,
-                options: q.options
-            })
-            nextIndex = nextIndex + 1;
-        } else {
-            alert("You finished assignment " + aID);
-            //API post with info
-            history.goBack();
-        }
-    };
+		// Future note: nextQuestion() is a missing dependency
+		// eslint-disable-next-line
+	}, [aID]);
 
-    const firstResponseCallback = (id, isAns) => {
-        const s = isAns ? 1 : 0;
-        score = score + s;
-    };
+	const nextQuestion = () => {
+		const qList = assignment.question;
+		if (nextIndex < qList.length()) {
+			const q = qList[nextIndex];
+			setQuestion({
+				id: "question" + nextIndex,
+				answer: q.answer,
+				question: q.question,
+				options: q.options,
+			});
+			nextIndex = nextIndex + 1;
+		} else {
+			alert("You finished assignment " + aID);
+			//API post with info
+			history.goBack();
+		}
+	};
 
-    const correctResponseCallback = (id) => {
-        nextQuestion();
-    };
+	const firstResponseCallback = (id, isAns) => {
+		const s = isAns ? 1 : 0;
+		score = score + s;
+	};
 
-    return (
-        <div style={styles.root}>
-            <Container style={styles.container}>
-                <QuestionAnswer qnSet={questionSet} title={title} subtitle={subtitle} onFirstResponse={firstResponseCallback} onCorrectResponse={correctResponseCallback}/>
-            </Container>
-        </div>
-    );
+	const correctResponseCallback = (id) => {
+		nextQuestion();
+	};
+
+	return (
+		<div style={styles.root}>
+			<Container style={styles.container}>
+				<QuestionAnswer
+					qnSet={questionSet}
+					title={title}
+					subtitle={subtitle}
+					onFirstResponse={firstResponseCallback}
+					onCorrectResponse={correctResponseCallback}
+				/>
+			</Container>
+		</div>
+	);
 }
--- src/components/Arena/AssignmentPanel.js
+++ src/components/Arena/AssignmentPanel.js
@@ -1,35 +1,47 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 
-import {TableStructure} from "../Common";
+import { TableStructure } from "../Common";
 
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 
-export default function() {
+export default function () {
+	const title = "Assignments";
+	const columns = [
+		{ title: "Title", field: "title" },
+		{ title: "Status", field: "status" }, //Submitted or not attempted
+		{ title: "Teacher", field: "teacher" }, //Teacher who gave out the assignment and is responsible for it
+	];
+	const data = [
+		//Replace with fetched data
+		{
+			title: "Multiplication",
+			status: "Submitted",
+			teacher: "Algebra teacher",
+		},
+		{ title: "Algebra", status: "Not attempted", teacher: "Algebra teacher" },
+		{ title: "Geometry", status: "Submitted", teacher: "Geometry teacher" },
+	];
+	const actions = [
+		{
+			icon: ArrowForwardIcon,
+			tooltip: "Start assignment",
+			onClick: (event, rowData) => {}, //Function to load assignment (iff not already submitted!)
+		},
+	];
+	const options = {
+		filtering: true, //Not sure we want this
+		sorting: true,
+		actionsColumnIndex: -1,
+	};
 
-    const title = "Assignments"
-    const columns = [
-        {title: 'Title', field: 'title'},
-        {title: 'Status', field: 'status'}, //Submitted or not attempted
-        {title: 'Teacher', field: 'teacher'} //Teacher who gave out the assignment and is responsible for it
-    ]
-    const data = [ //Replace with fetched data
-        {title: "Multiplication", status: "Submitted", teacher: "Algebra teacher"},
-        {title: "Algebra", status: "Not attempted", teacher: "Algebra teacher"},
-        {title: "Geometry", status: "Submitted", teacher: "Geometry teacher"}
-    ]
-    const actions = [
-        {
-            icon: ArrowForwardIcon,
-            tooltip: "Start assignment",
-            onClick: (event, rowData) => {} //Function to load assignment (iff not already submitted!)
-        }
-    ]
-    const options = {
-        filtering: true, //Not sure we want this
-        sorting: true,
-        actionsColumnIndex: -1
-    }
-
-    return <TableStructure title={title} columns={columns} data={data} actions={actions} options={options}/>;
+	return (
+		<TableStructure
+			title={title}
+			columns={columns}
+			data={data}
+			actions={actions}
+			options={options}
+		/>
+	);
 }
